0e3a85173c3fc83d725bdd7533ea9cf28023714e
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-11 19:12:27 -0600

    kussku

--- root/startup.tf
+++ root/startup.tf
@@ -1,47 +1,166 @@
-module "vpc" {
-  source = "../modules/vpc"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  cidr_block                 = var.vpc_cidr_block
-  azs                        = var.azs
-  public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
-  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
-  project                    = var.project
+  tags = {
+    Name = "${var.cluster_name}-vpc"
+  }
 }
 
-module "cluster" {
-  source = "../modules/iam/cluster"
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
 }
 
-module "node" {
-  source = "../modules/iam/node"
+resource "aws_subnet" "eks_subnets" {
+  count                   = length(var.subnet_cidrs)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                        = "${var.cluster_name}-subnet-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
 
-module "admin" {
-  source     = "../modules/iam/admin"
-  
-  depends_on = [module.eks]
+resource "aws_route_table" "eks_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-rt"
+  }
 }
 
-module "group" {
-  source = "../modules/iam/group"
+resource "aws_route_table_association" "eks_rta" {
+  count          = length(aws_subnet.eks_subnets)
+  subnet_id      = aws_subnet.eks_subnets[count.index].id
+  route_table_id = aws_route_table.eks_rt.id
 }
 
-module "eks" {
-  source = "../modules/eks"
+data "aws_availability_zones" "available" {}
 
-  role_arn           = module.cluster.role_arn
-  node_role_arn      = module.node.role_arn
-  private_subnets    = module.vpc.private_subnets
-  security_group_ids = [module.vpc.security_group_id]
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
 
-  depends_on = [module.vpc, module.cluster, module.node, module.group]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-module "asg" {
-  source = "../modules/asg"
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
 
-  security_group_ids   = [module.vpc.security_group_id]
-  apiServerEndpoint    = module.eks.apiServerEndpoint
-  certificateAuthority = module.eks.certificateAuthority
-  subnets              = module.vpc.public_subnets
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnets[*].id
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${self.name} --region ${var.region}"
+  }
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+data "aws_ssm_parameter" "eks_ami" {
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.eks_cluster.version}/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "eks_nodes" {
+  name                   = "${var.cluster_name}-node-template"
+  instance_type          = "t3.medium"
+  image_id               = data.aws_ssm_parameter.eks_ami.value
+  vpc_security_group_ids = [aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    set -o xtrace
+
+    /etc/eks/bootstrap.sh ${var.cluster_name}
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.cluster_name}-node"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "eks_nodes" {
+  name                = "${var.cluster_name}-nodes"
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  target_group_arns   = []
+  vpc_zone_identifier = aws_subnet.eks_subnets[*].id
+
+  launch_template {
+    id      = aws_launch_template.eks_nodes.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster_name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
 }
--- root/variables.tf
+++ root/variables.tf
@@ -1,19 +1,23 @@
-variable "vpc_cidr_block" {
-  type = string
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "azs" {
-  type = list(string)
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
 }
 
-variable "public_subnet_cidr_blocks" {
-  type = list(string)
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "private_subnet_cidr_blocks" {
-  type = list(string)
-}
-
-variable "project" {
-  type = string
+variable "subnet_cidrs" {
+  description = "CIDR blocks for subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
